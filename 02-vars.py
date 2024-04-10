e3464d9278dde8a0f02f63522b765fb52eb2672a
SVR Kalluru <raghuk.vit@gmail.com>
2024-04-10 08:00:03 +0530

    Python Code

---
+++ 02-vars.py
@@ -0,0 +1,25 @@
+x = 10 # integer
+y = 10.5 # float
+s = "DevOps" # string
+# single or double quote can be used
+
+# booleans
+b = True
+c = False
+
+# List
+l = [ 1, 2, "hello", True]
+# list can have different values with different data types.
+
+# Map
+m = {"course" : "DevOps", "timings": "6am"}
+
+# All the above variables are mutable
+
+# Immutables are Tuple
+t = [1,2,3]
+
+# Sets
+s = {1,2,3,3}
+
+print(s)
